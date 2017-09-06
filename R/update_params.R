31b57531284e494c0b2e4da9e589f202688c7c54
Kushal K Dey <kshldey@gmail.com>
2017-09-06 13:21:33 -0700

    update params

--- R/update_params.R
+++ R/update_params.R
@@ -6,17 +6,7 @@ update_params <- function(counts, omega, theta, metadata){
   
     nsamples <- nrow(counts)
     G <- ncol(counts)
-    num_covars <- ncol(metadata)
     K <- ncol(omega)
-    
-  
-   if(dim(omega)[1]  != dim(metadata)[1]){
-     stop("Number of rows in the metadata must match that of the grades of membership matrix omega")
-   }
-  
-   if(dim(counts)[1]  != dim(metadata)[1]){
-    stop("Number of rows in the metadata must match the number of rows in counts matrix")
-   }
   
    if(dim(theta)[2]  != dim(counts)[2]){
     stop("Number of columns in the theta matrix must match the number of columns in counts matrix")
@@ -27,7 +17,6 @@ update_params <- function(counts, omega, theta, metadata){
    }
   
     cluster_fac <- rep(1:K, each = nsamples)
-    metadata2 <- do.call(rbind, replicate(3, metadata, simplify=FALSE))
     
     library_size <- c(replicate(K, rowSums(counts)))
     tmp1 <- c(omega)
@@ -54,32 +43,55 @@ update_params <- function(counts, omega, theta, metadata){
     dim(latent_counts_array) <- c(nsamples, K, G)
     
     num_omega <- apply(latent_counts_array, c(1, 2), sum) + (1/K)
-    new_omega <- apply(num_omega, 1, function(x) return(x/sum(x)))
+    new_omega <- t(apply(num_omega, 1, function(x) return(x/sum(x))))
+    
+    if(!is.null(metadata)){
+      
+      num_covars <- ncol(metadata)
+      
+      if(dim(omega)[1]  != dim(metadata)[1]){
+        stop("Number of rows in the metadata must match that of the grades of membership matrix omega")
+      }
+      
+      if(dim(counts)[1]  != dim(metadata)[1]){
+        stop("Number of rows in the metadata must match the number of rows in counts matrix")
+      }
+      
+      metadata2 <- do.call(rbind, replicate(K, metadata, simplify=FALSE))
+      metadata_cluster <- model.matrix(~factor(cluster_fac)-1)
+      metadata3 <- cbind(metadata2, metadata_cluster)
+      
+      if(!is.null(colnames(metadata))){
+        colnames(metadata3) <- c(colnames(metadata), paste0("clus:", 1:K))
+      }else{
+        colnames(metadata3) <- c(paste0("meta:", 1:dim(metadata)[2]), paste0("clus:", 1:K))
+      }
+    }else{
+      metadata_cluster <- model.matrix(~factor(cluster_fac)-1)
+      metadata3 <- metadata_cluster
+      colnames(metadata3) <- paste0("clus:", 1:K)
+    }
     
     cl <- makeCluster(parallel::detectCores(),type=ifelse(.Platform$OS.type=="unix","FORK","PSOCK"))
     print(cl)
-    
-    dim(metadata2)
-    metadata_cluster <- model.matrix(~factor(cluster_fac)-1)
-    metadata3 <- cbind(metadata2, metadata_cluster)
-    if(!is.null(colnames(metadata))){
-      colnames(metadata3) <- c(colnames(metadata), paste0("clus:", 1:K))
-    }else{
-      colnames(metadata3) <- c(paste0("meta:", 1:dim(metadata)[2]), paste0("clus:", 1:K))
-    }
-    
-    fits <- distrom::dmr(cl, metadata3, latent_counts, verb=1)
+    suppressWarnings(fits <- distrom::dmr(cl, metadata3, latent_counts, verb=1))
     stopCluster(cl)
     
-    coef_fitted <- as.matrix(coef(fits))
+    suppressWarnings(coef_fitted <- as.matrix(coef(fits)))
     metadata4 <- cbind(rep(1, dim(counts_repped)[1]), metadata3)
     
     fitted_val <- exp(as.matrix(metadata4) %*% coef_fitted)
     fitted_val_2 <- t(apply(fitted_val, 1, function(x) return(x/sum(x))))
     new_theta <- fitted_val_2
     
-    ll <- list("new_theta" =  new_theta,
-               "new_omega" =  new_omega,
+    dist_omega <- mean((omega - new_omega)^2)
+    dist_theta <- mean((theta - new_theta)^2)
+    
+    cat("distance measure: omega - ", dist_omega, ":  theta - ", dist_theta, "\n");
+    
+    
+    ll <- list("theta" =  new_theta,
+               "omega" =  new_omega,
                "coefficients" = coef_fitted)
     return(ll)
 }
--- vignettes/trial_update_theta.R
+++ vignettes/trial_update_theta.R
@@ -9,16 +9,108 @@
  head(grid_metadata)
  
  nsamples <- 38
- K <- 3
+ K <- 5
  num_covars <- 2
  G <- 304
- omega <- gtools::rdirichlet(nsamples, alpha = rep(1/K, K))
- theta <- gtools::rdirichlet(nsamples*K, alpha = rep(1/(K*G), G))
+# omega <- gtools::rdirichlet(nsamples, alpha = rep(1/K, K))
+# theta <- gtools::rdirichlet(nsamples*K, alpha = rep(1/(K*G), G))
  metadata <- grid_metadata[,1:3]
  library(distrom)
  counts <- taxonomic_counts
- out <- update_theta(taxonomic_counts, omega, theta, metadata)
-
+ 
+ iter <- 1
+ maxiter <- 50
+ 
+ 
+ gom_fit <- CountClust::FitGoM(taxonomic_counts, K=5, tol=0.1)
+ 
+ theta_pre <- gom_fit$clust_5$theta
+ omega_pre <- gom_fit$clust_5$omega
+ 
+ theta_old <- matrix(0, nsamples*K, dim(theta_pre)[1])
+ for(m in 1:dim(theta_pre)[2]){
+   theta_old[(nsamples*(m-1)+1):(nsamples*(m)),] <- do.call(rbind, replicate(nsamples, theta_pre[,m], simplify=FALSE))
+ }
+ 
+ omega <- omega_pre
+ theta <- theta_old
+ 
+ for(iter in 1:maxiter){
+   out <- update_params(taxonomic_counts, omega, theta, metadata)
+   theta <- out$theta
+   omega <- out$omega
+   cat("We are at iteration, ", iter)
+ }
+ 
+ elevation_metadata=grid_metadata$Elevation;
+ east_west_dir = grid_metadata$WorE;
+ rownames(omega) <- 1:nrow(omega)
+ 
+ BlockStructure(omega, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ BlockStructure(omega_pre, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ 
+ 
+ metadata <- matrix(sample(100, nsamples*num_covars, replace = FALSE), nsamples, num_covars)
+ 
+ omega <- omega_pre
+ theta <- theta_old
+ 
+ for(iter in 1:maxiter){
+   out <- update_params(taxonomic_counts, omega, theta, metadata)
+   theta <- out$theta
+   omega <- out$omega
+   cat("We are at iteration, ", iter)
+ }
+ 
+ elevation_metadata=grid_metadata$Elevation;
+ east_west_dir = grid_metadata$WorE;
+ rownames(omega) <- 1:nrow(omega)
+ 
+ BlockStructure(omega, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ BlockStructure(omega_pre, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ 
+ 
+ omega <- omega_pre
+ theta <- theta_old
+ 
+ for(iter in 1:maxiter){
+   out <- update_params(taxonomic_counts, omega, theta, metadata=NULL)
+   theta <- out$theta
+   omega <- out$omega
+   cat("We are at iteration, ", iter)
+ }
+ 
+ elevation_metadata=grid_metadata$Elevation;
+ east_west_dir = grid_metadata$WorE;
+ rownames(omega) <- 1:nrow(omega)
+ 
+ BlockStructure(omega, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ BlockStructure(omega_pre, blocker_metadata = east_west_dir,
+                order_metadata = elevation_metadata,
+                yaxis_label = "Elevation",
+                levels_decreasing = FALSE)
+ 
+ 
  
  plot(out$coefficients[3,])
  
